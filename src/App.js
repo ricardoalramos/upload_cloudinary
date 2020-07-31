d5989941727f0b07b801b8d70b82e774cf0e0d03
Ricardo.Ramos <ricardoalramos@gmail.com>
2020-07-31 00:11:59 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'rramos');
+    setLoading(true);
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/imgsrramos/image/upload',
+      { method: 'POST', body: data }
+    );
+    const file = await res.json();
+    setImage(file.secure_url);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container center">
+      <h1>Upload Image</h1>
+      <input
+        type="file"
+        name="file"
+        placeholder="Escolha uma imagem"
+        onChange={uploadImage}
+      />
+      <br />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} style={{ width: '300px' }} />
+      )}
     </div>
   );
 }
